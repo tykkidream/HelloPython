3725c391f7bbd5955c2f629efd90daa055413651
Tykkidream <tykkidream@gmail.com>
2017-10-26 22:10:18 +0800

    学习单元测试

---
+++ syntax-lesson/syntax-lesson01/syntax-lesson01-chapter11/names_test.py
@@ -0,0 +1,22 @@
+import unittest
+from name_function import get_formatted_name
+from name_function import get_formatted_name2
+
+class NameTestCase(unittest.TestCase):
+
+    def test_first_last_name(self):
+        formatted_name = get_formatted_name("janis", "joplin")
+        self.assertEqual(formatted_name, "Janis Joplin")
+
+
+    def test_first_last_name2_1(self):
+        formatted_name = get_formatted_name2("janis", "joplin")
+        self.assertEqual(formatted_name, "Janis Joplin")
+
+
+    def test_first_last_name2_2(self):
+        formatted_name = get_formatted_name2("wolfgang", "mozart", "amadeus")
+        self.assertEqual(formatted_name, "Wolfgang Amadeus Mozart")
+
+
+unittest.main()
