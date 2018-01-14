7a8e8b7f366deeede39642075c7e9fe94cbce11f
Tykkidream <tykkidream@gmail.com>
2018-01-14 16:51:17 +0800

    自动计算数据

--- syntax-lesson/syntax-lesson03/scatter_squares.py
+++ syntax-lesson/syntax-lesson03/scatter_squares.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s = 200)
+plt.scatter(x_values, y_values, s = 40)
 
 plt.title("Square Numbers", fontsize = 24)
 plt.xlabel("Value", fontsize= 14)
 plt.ylabel("Square of Value", fontsize = 14)
 
 plt.tick_params(axis="both", which = "major", labelsize = 14)
+
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
