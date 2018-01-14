f16096738e76bfb4ffcfcd4f84843fa8ab7f1b8b
Tykkidream <tykkidream@gmail.com>
2018-01-14 16:37:22 +0800

    校正图形

--- syntax-lesson/syntax-lesson03/mpl_squares.py
+++ syntax-lesson/syntax-lesson03/mpl_squares.py
@@ -1,9 +1,10 @@
 
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
-plt.plot(squares, linewidth = 5)
+plt.plot(input_values, squares, linewidth = 5)
 
 plt.title("Square Number", fontsize = 24)
 
