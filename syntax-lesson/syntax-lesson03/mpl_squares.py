c4d0bf547176f4cdee9eb012d2180528d96ae13a
Tykkidream <tykkidream@gmail.com>
2018-01-14 16:05:25 +0800

    调整图表的样式

--- syntax-lesson/syntax-lesson03/mpl_squares.py
+++ syntax-lesson/syntax-lesson03/mpl_squares.py
@@ -3,5 +3,14 @@ import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
 
-plt.plot(squares)
+plt.plot(squares, linewidth = 5)
+
+plt.title("Square Number", fontsize = 24)
+
+plt.xlabel("Value", fontsize = 4)
+
+plt.ylabel("Square of Value", fontsize = 4)
+
+plt.tick_params(axis="both", labelsize = 14)
+
 plt.show()
