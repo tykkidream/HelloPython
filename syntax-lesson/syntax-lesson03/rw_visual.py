2de6ee34d0ddc819e8d70ab6bdf8c2be0f51046a
Tykkidream <tykkidream@gmail.com>
2018-01-15 00:10:58 +0800

    给点着色

--- syntax-lesson/syntax-lesson03/rw_visual.py
+++ syntax-lesson/syntax-lesson03/rw_visual.py
@@ -5,7 +5,9 @@ from random_walk import RandomWalk
 while True:
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s = 15)
+
+    point_numberrs = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numberrs, cmap=plt.cm.Blues, edgecolors="none", s = 15)
     plt.show()
 
     keep_running = input("Make anthor walk? (y/n):")
