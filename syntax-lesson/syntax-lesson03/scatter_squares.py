c64e5756d1f692277bf21adcd502b5fcd6cb67f6
Tykkidream <tykkidream@gmail.com>
2018-01-14 16:55:49 +0800

    使用颜色映射

--- syntax-lesson/syntax-lesson03/scatter_squares.py
+++ syntax-lesson/syntax-lesson03/scatter_squares.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolors="none", s = 40)
+plt.scatter(x_values, y_values, c = y_values, cmap = plt.cm.Blues, edgecolors="none", s = 40)
 
 plt.title("Square Numbers", fontsize = 24)
 plt.xlabel("Value", fontsize= 14)
