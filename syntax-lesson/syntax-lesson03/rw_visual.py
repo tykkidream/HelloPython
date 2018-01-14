00a0e6f0c14486898c48f6518557874139d46d88
Tykkidream <tykkidream@gmail.com>
2018-01-15 00:07:40 +0800

    模拟多冷随机漫步

--- syntax-lesson/syntax-lesson03/rw_visual.py
+++ syntax-lesson/syntax-lesson03/rw_visual.py
@@ -2,9 +2,12 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-rw = RandomWalk()
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, s = 15)
+    plt.show()
 
-rw.fill_walk()
-
-plt.scatter(rw.x_values, rw.y_values, s = 15)
-plt.show()
+    keep_running = input("Make anthor walk? (y/n):")
+    if keep_running == "n":
+        break
