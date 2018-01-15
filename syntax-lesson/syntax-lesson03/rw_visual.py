346d0e4195b9f45027d7fb1caa9c245f2ace957f
Tykkidream <tykkidream@gmail.com>
2018-01-16 00:02:14 +0800

    隐藏坐标轴

--- syntax-lesson/syntax-lesson03/rw_visual.py
+++ syntax-lesson/syntax-lesson03/rw_visual.py
@@ -12,6 +12,9 @@ while True:
     plt.scatter(0, 0, c="green", edgecolors="none", s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=100)
 
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make anthor walk? (y/n):")
