e3322be36cf4e52c60ef1ad5d26203f52c98689d
Tykkidream <tykkidream@gmail.com>
2018-01-16 00:06:12 +0800

    增加点数

--- syntax-lesson/syntax-lesson03/rw_visual.py
+++ syntax-lesson/syntax-lesson03/rw_visual.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     point_numberrs = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numberrs, cmap=plt.cm.Blues, edgecolors="none", s = 15)
+    plt.scatter(rw.x_values, rw.y_values, c=point_numberrs, cmap=plt.cm.Blues, edgecolors="none", s = 1)
 
     plt.scatter(0, 0, c="green", edgecolors="none", s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=100)
