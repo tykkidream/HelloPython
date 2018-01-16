93fa1f58dc45c298da1e9bbb9723256454abc880
Tykkidream <tykkidream@gmail.com>
2018-01-16 22:15:29 +0800

    分析结果

--- syntax-lesson/syntax-lesson03/die_visual.py
+++ syntax-lesson/syntax-lesson03/die_visual.py
@@ -4,8 +4,13 @@ die = Die()
 
 results = []
 
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
